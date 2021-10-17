11d1120f6f928d739975c1386c2f41e3d097fa93
SAIF AHMED <SAHMED@KINETICA.COM>
2021-10-17 11:34:50 -0400

    Includes lIEX based re-cache

--- recache.py
+++ recache.py
@@ -4,13 +4,13 @@ import io
 import json
 import logging
 
-import pyEX as p
-
 CACHE_FILE = "cache.csv"
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 def pull_quotes():
+	import pyEX as p
+
 	logging.info('Connecting to IEX for price cache')
 	c = p.Client()
 	tickers_all = p.symbolsList()
@@ -36,25 +36,36 @@ def pull_quotes():
 		)
 		results.append(ret)
 
+	dt = datetime.datetime.now() 
+	dt_string = dt.strftime("%Y_%m_%d__%H_%M_%S")
+	ts_filename = f"quotes_cache___{dt_string}.json"
 
 	results_merged = {}
 	for r in results:
 		for k in r.keys():
 			results_merged[k]=r[k]
 
+	push_cache(results_merged, cache_file=ts_filename)
+
 	return results_merged
+
+def pull_cache(cache_file=CACHE_FILE):
+	logging.info(f'Pulling local price cache from {cache_file}')
+	with open(cache_file, "r") as read_file:
+	    cache = json.load(read_file)
+	return cache
+
+def push_cache(quotes_cache, cache_file=CACHE_FILE):
+	logging.info(f'Pushing local price cache to {cache_file}')
+	with open(CACHE_FILE,'w') as f:    
+		json.dump(quotes_cache, f, indent=4)
 
 def main():
 
 	try:
-
-		results_merged = pull_quotes()
-
-		with open(CACHE_FILE,'w') as twitterDataFile:    
-			json.dump(results_merged, twitterDataFile, indent=4)
-
-		logging.info(f"Found {len(results_merged)} tickers to pull")
-
+		fresh_quotes = pull_quotes()
+		push_cache(fresh_quotes)
+		logging.info(f"Found {len(fresh_quotes)} tickers to pull")
 	except:
 		print("Problems")
 		
--- stream.py
+++ stream.py
@@ -7,11 +7,14 @@ import datetime
 import json
 import copy
 import logging
+import argparse
 
 from datetime import timezone
 from time import strftime
 
 from recache import pull_quotes
+from recache import pull_cache
+
 
 from kafka import KafkaProducer
 
@@ -42,28 +45,30 @@ producer = KafkaProducer(bootstrap_servers=[f"{KAFKA_BRKR}:{KAFKA_PORT}"],
 	json.dumps(x).encode('utf-8'))
 logging.info('Connecting to Kafka, completed')
 
-px_initial = {}
-results_merged = pull_quotes()
-
-with open(SEED_FILE, "r", encoding='utf-8-sig') as my_file:
-	content = my_file.read()
-	quotes = content.split("\n")
-	for q in quotes:
-		ticker, pxinit = q.split(",")
-		px_initial[ticker] = float(pxinit)
-px_now = copy.deepcopy(px_initial)
-
-def main():
+def stream(from_cache=True, from_exchange=False):
 	
-
+	if from_exchange:
+		logging.info("Pulling quotes from exchange")
+		#all_quotes = pull_cache()
+		all_quotes = pull_quotes()
+	if from_cache:
+		logging.info("Pulling quotes from cache")
+		all_quotes = pull_cache()
+	
+	px_now = {}
+	for s in all_quotes:
+		if "quote" in all_quotes[s]:
+			if "latestPrice" in all_quotes[s]["quote"]:
+				if all_quotes[s]["quote"]["latestPrice"]:
+					px_now[s] = all_quotes[s]["quote"]["latestPrice"]
+	
 	while True:
 		
 		time.sleep(TIME_BETWEEN_LOOPS)
 
-		q=[]
 		push_stamp = datetime.datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S") #.isoformat()
 		for c in px_now.keys():
-			#print(f"Handling ticker: {c}")
+			#print(f"Handling ticker: {c} with price {px_now[c]}")
 			newprice = px_now[c] + (random.random()-0.5)/2
 			px_now[c] = round(newprice, 2)
 			msg = {
@@ -72,12 +77,18 @@ def main():
 				"px_timestamp": push_stamp,
 				"size": int(random.random() * 10000)
 			}
-			q.append(msg)
-			producer.send(KAFKA_TOPIC, value=msg)
+			producer.send(KAFKA_TOPIC, value=msg, key=c)
 		producer.flush()
 
 		print(f"{push_stamp}   Tick!  Disney: {px_now['DIS']} \t AIG: {px_now['AIG']} \t Goldman Sachs: {px_now['GS']} \t Accenture: {px_now['ACN']}")
 		
-		
 if __name__ == "__main__":
-    main()
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--from-cache', action='store_true')
+	parser.add_argument('--from-exchange', action='store_true')
+	argx = parser.parse_args()
+	if (argx.from_cache and argx.from_exchange):
+		logging.error("Cannot operate both from-exchange and from-cache simultaneously")
+		sys.exit()
+	stream(from_cache=argx.from_cache, from_exchange=argx.from_exchange)
